ef1cc7404daa83d981d9f5df2fb905bd57b98afb
Yuxuan Lin <u6828533@anu.edu.au>
2020-10-09 11:29:45 +1100

    Implement a Tokenizer.

---
+++ app/src/main/java/com/example/cars4sale/Tokenizer.java
@@ -0,0 +1,14 @@
+package com.example.cars4sale;
+
+public abstract class Tokenizer {
+
+    // extract next token from the current text and save it
+    public abstract void next();
+
+    // return the current token (without type information)
+    public abstract Object current();
+
+    //check whether there is a token remaining in the text
+    public abstract boolean hasNext();
+
+}
