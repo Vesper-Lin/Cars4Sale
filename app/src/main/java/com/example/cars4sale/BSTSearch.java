4e9674f57276108fe97118b374816f19153904d6
Xinxin Li <u6750982@anu.edu.au>
2020-10-18 15:00:21 +0800

    Merge branch 'master' of /Users/lixinxin/Desktop/comp2100_6442_s2_2020_group_project with conflicts.

--- app/src/main/java/com/example/cars4sale/BSTSearch.java
+++ app/src/main/java/com/example/cars4sale/BSTSearch.java
@@ -126,7 +126,7 @@ public class BSTSearch extends BST {
             //System.out.println(list_to_map(map, list_to_list(getHigherPrice(node(sList).root, 988934))));
             //System.out.println(list_to_map(map,list_to_list(getLowerPrice(node(sList).root,537))).size());
             //System.out.println(getLocation(map,"Toowoomba"));
-            // System.out.println(getName(map,"SATURN Outlook"));
+             //System.out.println(getName(map,"SATURN Outlook"));
             // System.out.println(getYearBefore(map,2010));
             // System.out.println(getName(map, "SaTuRn"));
 
--- app/src/main/java/com/example/cars4sale/Parser.java
+++ app/src/main/java/com/example/cars4sale/Parser.java
@@ -1,50 +1,31 @@
 package com.example.cars4sale;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * The main objective of this class is to implement a parser for the search query.
- * It should be able to parser the following grammar rule:
- * <exp>    ::= <term> ; <exp> | <term>
- * <term>   ::= <key> <data>
- * <key>    ::= "name=" | "location=" | "price<" | "price>" | "year=" | "year<" | "year>"
- * <data>   ::= <unsigned string>
+ *  The main objective of this class is to implement a parser for the search query.
+ *  It should be able to parser the following grammar rule:
+ *  <exp>    ::= <term> ; <exp> | <term>
+ *  <term>   ::= <key> <data>
+ *  <key>    ::= "name=" | "location=" | "price=" | "price<" | "price>" | "year=" | "year<" | "year>"
+ *  <data>   ::= <unsigned string>
+ *
  */
 public class Parser {
 
-    /**
-     * Parser for <exp>.
-     * <exp> has two production rules: <term> ; <exp> | <term>
-     * If there's ; token after parsing term, the parser return an and expression
-     * otherwise it returns parsed term.
-     *
-     * @param tok Tokenizer
-     * @return parsed expression for <exp>
-     */
-    public static Exp parseExp(Tokenizer tok) {
+    MyTokenizer _tokenizer;
 
-        Exp term = parseTerm(tok);
-
-        if (tok.hasNext() && tok.current().equals(";")) {
-            tok.next();
-            Exp exp = parseExp(tok);
-            return new ExpAnd(term, exp);
-        } else {
-            return term;
-        }
+    
+    public Parser(MyTokenizer tokenizer) {
+        _tokenizer = tokenizer;
     }
 
-    public static Exp parseTerm(Tokenizer tok) {
-        Exp exp1 = (Exp) tok.current();
-        tok.next();
-        Exp exp2 = (Exp) tok.current();
-        tok.next();
-        Exp exp3 = (Exp) tok.current();
-        Exp test = new ExpTerm(exp1, exp2, exp3);
-        tok.next();
-        return test;
+    public Exp parseExp(){
+        return null;
     }
 
-
+    public static void main(String[] args) {
+        MyTokenizer mathTokenizer = new MyTokenizer("location = canberra; price < 100000 ; year > 2000");
+        Exp t1 = new Parser(mathTokenizer).parseExp();
+        System.out.println(t1.show());
+        System.out.println(t1.evaluate());
+    }
 }
