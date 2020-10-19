4089f06b9268684d6825649a21bbcf85d8f9c690
Yuxuan Lin <u6828533@anu.edu.au>
2020-10-19 18:11:24 +1100

    Update the context-free grammar.

--- app/src/main/java/com/example/cars4sale/Parser/Parser.java
+++ app/src/main/java/com/example/cars4sale/Parser/Parser.java
@@ -1,27 +1,24 @@
 package com.example.cars4sale.Parser;
 
-import com.example.cars4sale.Tokenizer.Tokenizer;
-
 /**
  * The main objective of this class is to implement a parser for the search query.
- * It should be able to parser the following grammar rule:
- * <exp>    ::= <term> ; <exp> | <term>
- * <term>   ::= <key> <data>
- * <key>    ::= "name=" | "location=" | "price<" | "price>" | "year=" | "year<" | "year>"
- * <data>   ::= <unsigned string>
+ * It should be able to parser the following context-free grammar rule:
+ * <exp>        ::=   <term> | <term> ; <exp>
+ * <term>       ::=   <keyword> <comparison> <value>
+ * <value>      ::=   <unsigned string> | <unsigned integer>
+ * <keyword>    ::=   "name" | "location" | "price" | "year"
+ * <comparison> ::=   "=" | "<" | ">"
+ *
+ * @author: Yuxuan Lin
+ * @UID: u6828533
  */
 public class Parser {
 
     /**
-     * Parser for <exp>.
-     * <exp> has two production rules: <term> ; <exp> | <term>
-     * If there's ; token after parsing term, the parser return an and expression
-     * otherwise it returns parsed term.
-     *
-     * @param tok Tokenizer
-     * @return parsed expression for <exp>
+     * Parse the queries with the form of
+     * <exp> ::= <term> | <term> ; <exp>
      */
-    public static Exp parseExp(Tokenizer tok) {
+    public static Exp parseExp() {
         return null;
     }
 }
