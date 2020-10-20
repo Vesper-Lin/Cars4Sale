cd09108715e5f88c8bfe432e585b81fa651b2d5f
tianxianzhang <u6773547@anu.edu.au>
2020-10-20 16:57:07 +1100

    Defined Layout on ListView

--- app/src/main/java/com/example/cars4sale/ActivityWeb.java
+++ app/src/main/java/com/example/cars4sale/ActivityWeb.java
@@ -6,11 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.example.cars4sale.DataGenerator.Car;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 
 public class ActivityWeb extends AppCompatActivity {
     private Button button;
@@ -29,11 +28,19 @@ public class ActivityWeb extends AppCompatActivity {
 
         // Settings of Adapter
         // SimpleAdapter(Context context, List < ? extends Map<String, ?>>data,int resource, String[] from,int[] to)
-        simple_adapter = new SimpleAdapter(this, list, R.layout.lvcar, from, to);
+
+
+        // List<String extends Map<String, Object>> arrarylist;
+        List<String>
+        simple_adapter = new SimpleAdapter(this, arraylist, R.layout.activity_web, from, to);
+        list = new arraylist();
+        for (Car c : map.values)list.add (c name) {
+
+        }
 
         // Start Adapter
         lvcar.setAdapter(simple_adapter);
-*/
+
         button = (Button)findViewById(R.id.button_a);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,18 +48,17 @@ public class ActivityWeb extends AppCompatActivity {
                 openMainActivity();
             }
         });
-/*
+
         List<String> mapValuesToList = object.mapValuesToList(hashMap);
         mapValuesToList.forEach(System.out::println);
-*/
+
     }
 
     public List<String> mapValuesToList(Map<String,String> map) {
         Collection<String> values = map.values();
         ArrayList<String> arrayList = new ArrayList<>(values);
         return arrayList;
-    }
-
+*/    }
     public void openMainActivity(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
