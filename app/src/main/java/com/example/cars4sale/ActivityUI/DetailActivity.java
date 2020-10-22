61c254063d6245c41d975baa13cf10bae7fa8f00
Yuxuan Lin <u6828533@anu.edu.au>
2020-10-23 07:18:25 +1100

    Fix gradle.

--- app/src/main/java/com/example/cars4sale/ActivityUI/DetailActivity.java
+++ app/src/main/java/com/example/cars4sale/ActivityUI/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.example.cars4sale.ActivityUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.cars4sale.R;
 
@@ -11,6 +14,25 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.detail_main);
+
+        // Navigation drawer
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+
+        String[] carDetails = intent.getStringExtra("CarDetails").split("\\n");
+
+        final TextView name = findViewById(R.id.textName);
+        final TextView price = findViewById(R.id.textPrice);
+        final TextView location = findViewById(R.id.textLocation);
+        final TextView year = findViewById(R.id.textYear);
+
+        name.setText(carDetails[0]);
+        price.setText(carDetails[1]);
+        location.setText(carDetails[2]);
+        year.setText(carDetails[3]);
+
     }
 }
